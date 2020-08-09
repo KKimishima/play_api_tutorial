0a10a17d340139da3e359f09226b757520c3faea
KKimishima <k.kimishima123@gmail.com>
2020-08-09 12:32:35 +0900

    <追加>/helloにpostにjson機能追加

--- app/controllers/HelloController.scala
+++ app/controllers/HelloController.scala
@@ -1,9 +1,18 @@
 package controllers
 
 import javax.inject.Inject
+import play.api.libs.json.Json
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
+
+import scala.util.chaining.scalaUtilChainingOps
 
 
 class HelloController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
   def hello(): Action[AnyContent] = Action(Ok("Hello world"))
+
+  def helloJson(): Action[AnyContent] = Action {
+    Json.obj("hello" -> "world", "language" -> "scala").pipe { it =>
+      Ok(it)
+    }
+  }
 }
--- test/controllers/HelloControllerSpec.scala
+++ test/controllers/HelloControllerSpec.scala
@@ -2,6 +2,7 @@ package controllers
 
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerTest
+import play.api.libs.json.Json
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 
@@ -13,6 +14,19 @@ class HelloControllerSpec extends PlaySpec with GuiceOneAppPerTest {
       status(res) mustBe OK
       contentType(res) mustBe Some("text/plain")
       contentAsString(res) mustBe "Hello world"
+    }
+  }
+
+  "HelloController POST" must {
+    "/hello post でjsonが返る" in {
+      val req = FakeRequest(POST, "/hello")
+      val res = route(app, req).get
+      status(res) mustBe OK
+      contentType(res) mustBe Some("application/json")
+      contentAsJson(res) mustBe Json.obj(
+        "hello" -> "world",
+        "language" -> "scala"
+      )
     }
   }
 }

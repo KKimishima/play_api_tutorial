5a08612b5e9d0e4e0da6bd571c57eb3339e5c2e4
KKimishima <k.kimishima123@gmail.com>
2020-08-09 11:45:55 +0900

    <追加>/hello エンドポイント追加

---
+++ test/controllers/HelloControllerSpec.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneAppPerTest
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+
+class HelloControllerSpec extends PlaySpec with GuiceOneAppPerTest {
+  "HelloControllerSpec GET" must {
+    "/hello get　にアクセスできる" in {
+      val req = FakeRequest(GET, "/hello")
+      val res = route(app, req).get
+      status(res) mustBe OK
+    }
+  }
+}
